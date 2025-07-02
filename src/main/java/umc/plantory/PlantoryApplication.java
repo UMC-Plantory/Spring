98ef4d9d82c2153f87fa92878b7f26eaa4a1a18f
isee <stpark0509@gmail.com>
2025-07-02 18:03:02 +0900

    🎉 [Chore] 프로젝트 초기 세팅

---
+++ src/main/java/umc/plantory/PlantoryApplication.java
@@ -0,0 +1,13 @@
+package umc.plantory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantoryApplication.class, args);
+	}
+
+}
