495e9fce13792ab439d5fad96db5b6c52cbf9ee8
gayo73 <163620291+gayo73@users.noreply.github.com>
2025-07-29 15:39:41 +0900

    [Refactor] 유저 controller service 의존 관계 변경

--- src/main/java/umc/plantory/domain/member/controller/MemberRestController.java
+++ src/main/java/umc/plantory/domain/member/controller/MemberRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import umc.plantory.domain.member.dto.MemberRequestDTO;
 import umc.plantory.domain.member.dto.MemberResponseDTO;
 import umc.plantory.domain.member.service.MemberCommandService;
+import umc.plantory.domain.member.service.MemberCommandUseCase;
 import umc.plantory.global.apiPayload.ApiResponse;
 
 @RestController
@@ -15,19 +16,19 @@ import umc.plantory.global.apiPayload.ApiResponse;
 @RequiredArgsConstructor
 @Tag(name = "Member", description = "회원 관련 API")
 public class MemberRestController {
-    private final MemberCommandService memberCommandService;
+    private final MemberCommandUseCase memberCommandUseCase;
 
     @PostMapping("/term")
     @Operation(summary = "약관 동의 API", description = "회원이 약관에 동의하는 API입니다.")
     public ResponseEntity<ApiResponse<MemberResponseDTO.TermAgreementResponse>> termAgreement(
             @RequestBody MemberRequestDTO.TermAgreementRequest request) {
-        return ResponseEntity.ok(ApiResponse.onSuccess(memberCommandService.termAgreement(request)));
+        return ResponseEntity.ok(ApiResponse.onSuccess(memberCommandUseCase.termAgreement(request)));
     }
 
     @PatchMapping("/signup")
     @Operation(summary = "회원가입 완료 API", description = "회원의 추가 정보(닉네임, 사용자 커스텀 ID, 성별, 생년월일, 프로필 이미지)를 입력하여 회원가입을 완료하는 API입니다.")
     public ResponseEntity<ApiResponse<MemberResponseDTO.MemberSignupResponse>> signup(
             @RequestBody MemberRequestDTO.MemberSignupRequest request) {
-        return ResponseEntity.ok(ApiResponse.onSuccess(memberCommandService.memberSignup(request)));
+        return ResponseEntity.ok(ApiResponse.onSuccess(memberCommandUseCase.memberSignup(request)));
     }
 }
