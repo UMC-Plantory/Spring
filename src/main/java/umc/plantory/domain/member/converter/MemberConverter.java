83e0828e52b9948d4f2fea93b7e41b23214f9046
emfo-iseevict <pst@emfoplus.co.kr>
2025-07-29 13:31:52 +0900

    [Refactor] default 값 관련 수정

--- src/main/java/umc/plantory/domain/member/converter/MemberConverter.java
+++ src/main/java/umc/plantory/domain/member/converter/MemberConverter.java
@@ -8,15 +8,15 @@ import umc.plantory.global.enums.MemberStatus;
 import umc.plantory.global.enums.Provider;
 
 public class MemberConverter {
-    private static final String defaultNickname = "토리";
-    private static final String defaultProfileImg = "";
+    private static final String DEFAULT_NICKNAME = "토리";
+    private static final String DEFAULT_PROFILE_IMG = "https://plantory.s3.ap-northeast-2.amazonaws.com/profile/plantory_default_img.png";
 
     public static Member toMember (MemberDataDTO.KakaoMemberData kakaoMemberData) {
         return Member.builder()
-                .nickname(defaultNickname)
+                .nickname(DEFAULT_NICKNAME)
                 .email(kakaoMemberData.getEmail())
                 .userCustomId("")
-                .profileImgUrl(defaultProfileImg)
+                .profileImgUrl(DEFAULT_PROFILE_IMG)
                 .provider(Provider.KAKAO)
                 .providerId(kakaoMemberData.getSub())
                 .gender(Gender.NONE)
