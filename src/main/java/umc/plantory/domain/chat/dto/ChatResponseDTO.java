2de8121848c5f209442814941f903d9ca8ebc5f0
parkhyungjin <gud5223@naver.com>
2025-08-04 15:23:09 +0900

    [FEAT] DTO 형식 통일

---
+++ src/main/java/umc/plantory/domain/chat/dto/ChatResponseDTO.java
@@ -0,0 +1,39 @@
+package umc.plantory.domain.chat.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.ai.chat.messages.MessageType;
+
+import java.time.LocalDateTime;
+
+public class ChatResponseDTO {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ChatResponse {
+        @Schema(description = "메시지 내용")
+        String content;
+        @Schema(description = "생성 시간", example = "2025-07-20T12:00:00")
+        LocalDateTime createAt;
+        @Schema(description = "사용자 요청인지 챗봇 응답인지", example = "USER")
+        MessageType messageType;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GeneratedResponse {
+        @Schema(description = "메시지 내용")
+        String content;
+        @Schema(description = "생성 시간", example = "2025-07-20T12:00:00")
+        LocalDateTime createAt;
+        @Schema(description = "사용자 요청인지 챗봇 응답인지", example = "USER")
+        MessageType messageType;
+    }
+}
