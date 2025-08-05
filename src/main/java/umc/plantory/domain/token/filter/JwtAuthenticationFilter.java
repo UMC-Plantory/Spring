1affc6d56445e377f87b5316d3fa52190785342c
emfo-iseevict <pst@emfoplus.co.kr>
2025-08-05 17:53:51 +0900

    [Refactor] OAuth2 로그인 로직 변경 - Spring Security 적용 - 의존성 변경 - memberId 추출 로직 통합

---
+++ src/main/java/umc/plantory/domain/token/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package umc.plantory.domain.token.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import umc.plantory.domain.token.provider.JwtProvider;
+import umc.plantory.global.security.CustomUserDetails;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 헤더에서 JWT 추출
+        String token = jwtProvider.resolveToken(request.getHeader("Authorization"));
+
+        if (token != null && jwtProvider.validateToken(token)) {
+            Long memberId = jwtProvider.getMemberId(token);
+
+            CustomUserDetails userDetails = new CustomUserDetails(memberId);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
