81ed8a73db365874380a3e04b6da3f25b0a08524
parkhyungjin <gud5223@naver.com>
2025-08-09 01:36:12 +0900

    [Refactor] ChatRequestDTO 단일 클래스로 통합

--- src/main/java/umc/plantory/domain/chat/dto/ChatRequestDTO.java
+++ src/main/java/umc/plantory/domain/chat/dto/ChatRequestDTO.java
@@ -1,19 +1,16 @@
 package umc.plantory.domain.chat.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 public class ChatRequestDTO {
-
-    @Getter
-    public static class ChatMessageDTO {
-
-        @Schema(description = "채팅 내용", example = "오늘 너무 우울해", maxLength = 400)
-        @NotNull(message = "채팅 내용은 필수입니다")
-        private String content;
-    }
-
+    @Schema(description = "채팅 내용", example = "오늘 너무 우울해", maxLength = 400)
+    @NotNull(message = "채팅 내용은 필수입니다")
+    private String content;
 }
