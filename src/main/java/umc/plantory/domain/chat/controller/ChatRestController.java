915a0740e023ccf2f84f7b2688cbe137a661ad1b
parkhyungjin <gud5223@naver.com>
2025-08-09 02:34:15 +0900

    [Refactor] 컨트롤러 태그 추가 및 일관성 처리

--- src/main/java/umc/plantory/domain/chat/controller/ChatRestController.java
+++ src/main/java/umc/plantory/domain/chat/controller/ChatRestController.java
@@ -1,6 +1,7 @@
 package umc.plantory.domain.chat.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 /** 심현민 & 박형진 공동 작업 **/
+@Tag(name = "Chat", description = "채팅 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/plantory/chat")
@@ -23,11 +25,11 @@ public class ChatRestController {
     private final ChatQueryUseCase chatQueryUseCase;
     private final ChatCommandUseCase chatCommandUseCase;
 
-    @PostMapping
     @Operation(
             summary = "챗봇 채팅 요청",
             description = "사용자가 챗봇에게 메시지를 보내면, 챗봇의 답변을 반환합니다."
     )
+    @PostMapping
     public ResponseEntity<ApiResponse<ChatResponseDTO>> chat (
             @RequestBody @Valid ChatRequestDTO request,
             @RequestHeader(value = "Authorization", required = false) String authorization
@@ -36,24 +38,23 @@ public class ChatRestController {
         return ResponseEntity.ok(ApiResponse.onSuccess(response));
     }
 
-    // 챗봇 채팅창 처음 최초 진입 : 최신 6개
-    @GetMapping("/latest")
     @Operation(
             summary = "챗봇 채팅창 이전 대화 기록 최초 조회",
             description = "사용자가 챗봇과 나눈 이전 대화 중 가장 최근 6개를 조회합니다. "
     )
+    @GetMapping("/latest")
     public ResponseEntity<ApiResponse<List<ChatResponseDTO>>> getChatHistoryLatest (
             @RequestHeader(value = "Authorization", required = false) String authorization
     ) {
         List<ChatResponseDTO> latestChats = chatQueryUseCase.findLatestChats(authorization);
         return ResponseEntity.ok(ApiResponse.onSuccess(latestChats));
     }
-    // 최초 진입 이후, 스크롤 업: 특정 createdAt 이전 6개 조회 (커서 페이징)
-    @GetMapping("/before")
+
     @Operation(
             summary = "최초 진입 이후, 챗봇 채팅창 이전 채팅 스크롤 조회",
             description = "스크롤 시, 기준 시각(before) 이전의 6개 채팅을 추가로 조회 "
     )
+    @GetMapping("/before")
     public ResponseEntity<ApiResponse<List<ChatResponseDTO>>> getChatHistoryBefore (
             @RequestHeader(value = "Authorization", required = false) String authorization,
             @RequestParam("before") LocalDateTime before
