170e73507ab566b0ef7f822d316eda6326eb426b
parkhyungjin <gud5223@naver.com>
2025-08-09 01:39:35 +0900

    [Feat] toChatResponseDTO 생성하는 컨버터 구현

--- src/main/java/umc/plantory/domain/chat/converter/ChatConverter.java
+++ src/main/java/umc/plantory/domain/chat/converter/ChatConverter.java
@@ -1,6 +1,7 @@
 package umc.plantory.domain.chat.converter;
 
 import org.springframework.ai.chat.messages.MessageType;
+import umc.plantory.domain.chat.dto.ChatResponseDTO;
 import umc.plantory.domain.chat.entity.Chat;
 import umc.plantory.domain.member.entity.Member;
 
@@ -12,6 +13,14 @@ public class ChatConverter {
                 .content(message)
                 .isMember(isMember)
                 .messageType(type)
+                .build();
+    }
+
+    public static ChatResponseDTO toChatResponseDTO(Chat chat) {
+        return ChatResponseDTO.builder()
+                .content(chat.getContent())
+                .createdAt(chat.getCreatedAt())
+                .isMember(chat.getIsMember())
                 .build();
     }
 
