d50dc8d3ac5fd81e693add3e512a7262057a42e4
parkhyungjin <gud5223@naver.com>
2025-08-09 02:27:35 +0900

    [Feat] 채팅 입력 검증 추가(@NotBlank, @Size(500))

--- src/main/java/umc/plantory/domain/chat/dto/ChatRequestDTO.java
+++ src/main/java/umc/plantory/domain/chat/dto/ChatRequestDTO.java
@@ -1,16 +1,14 @@
 package umc.plantory.domain.chat.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotNull;
 
 @Getter
 public class ChatRequestDTO {
     @Schema(description = "채팅 내용", example = "오늘 너무 우울해", maxLength = 500)
-    @NotNull(message = "채팅 내용은 필수입니다")
+    @NotBlank(message = "채팅 내용은 필수입니다")
+    @Size(max = 500, message = "채팅 내용은 500자 이하여야 합니다")
     private String content;
 }
