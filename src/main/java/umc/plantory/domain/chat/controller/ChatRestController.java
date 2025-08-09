77b7e699bcfa84ab86efa0bd6277b27a33e79ee8
parkhyungjin <gud5223@naver.com>
2025-08-09 19:31:00 +0900

    [Fix] 도커 이미지 다시 빌드

--- src/main/java/umc/plantory/domain/chat/controller/ChatRestController.java
+++ src/main/java/umc/plantory/domain/chat/controller/ChatRestController.java
@@ -14,6 +14,7 @@ import umc.plantory.global.apiPayload.ApiResponse;
 
 import java.time.LocalDateTime;
 import java.util.List;
+//
 
 /** 심현민 & 박형진 공동 작업 **/
 @Tag(name = "Chat", description = "채팅 관련 API")
